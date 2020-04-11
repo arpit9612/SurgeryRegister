fa25fd7ea8ca27eb9db51821640b242ddaa11ff4
arpit trivedi <35517286+arpit9612@users.noreply.github.com>
2020-04-11 12:09:59 +0530

    change in UI

--- src/app/name-editor/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor/name-editor.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit,Input} from '@angular/core';
-import { FormGroup ,FormBuilder ,Validators} from "@angular/forms";
+import { FormGroup ,FormBuilder ,Validators,FormControl, NgForm} from "@angular/forms";
 import { MatRadioModule } from "@angular/material/radio";
 import { MatSelectModule } from "@angular/material/select";
+import { strict } from 'assert';
+import { NgForOf } from '@angular/common';
 interface surgeon {
-  value: string;
+  id: string;
+  viewValue: string;
+}
+
+interface surgerySite{
+
+  id: string;
+  viewValue: string;
+}
+
+interface antibiotic{
+  id: string;
   viewValue: string;
 }
 
@@ -13,21 +26,61 @@ interface surgeon {
   styleUrls: ['./name-editor.component.scss']
 })
 export class NameEditorComponent implements OnInit{
+  
+ 
+  constructor(private fb : FormBuilder){  }
+
+  SurgeryForm = this.fb.group({
+    tagnumber :[''],
+    emergencynumber:[''],
+    animaltype:[''],
+    surgerydate:[''],
+    surgeon:[''],
+    anesthesia:[''],
+    stype:[''],
+    deathdate:[''],
+    deathcomment:[''],
+    antibiotics:[''],
+    comment:['']
+
+
+
+  });
+
+
+  surgeryType : string;
+    types: string[] = ['Spay','Neuter','Tumor Removal','Ceasarian Section','Leg Amputation','Tail Amputation','Haematoma','Wound Repair','Bone Fracture Repair','Eye Surgery','Jaw Repair','Hernia Repair','Exploratory Surgery Abdominal'];
+
+  surgeons: surgeon[] = [
+    {id: 'vaibhav', viewValue: 'Vaibhav'},
+    {id: 'other', viewValue: 'Other'},
+  ];
+
+  sites : surgerySite[] = [
+    {id: 'abccenter-0' , viewValue: 'ABC Center'},
+    {id: 'mainhospital-1' , viewValue: 'Main Hospital'},
+  ];
+  drugs  : antibiotic[] = [
+    {id: 'yes' , viewValue: 'Yes'},
+    {id: 'no' , viewValue: 'NO'},
+  ];
+
+    
   ngOnInit(){
     
   }
 
-  favoriteSeason: string;
-  seasons: string[] = ['Spay','Neuter','Tumor Removal','Ceasarian Section','Leg Amputation','Tail Amputation','Haematoma','Wound Repair','Bone Fracture Repair','Eye Surgery','Jaw Repair','Hernia Repair','Exploratory Surgery Abdominal','Other'];
+  Type(value)
+  {
+    console.log(value);
+  }
 
-  surgeons: surgeon[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-    {value: 'none-3', viewValue: 'None'}
-  ];
-    constructor(){}
-  
+  Submit(SurgeryForm:any)
+  {
+      console.log(SurgeryForm.value);
+
+  }
+
  
  
 
